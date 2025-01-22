29b92d12e87c71071591be2d9a06c10bdc238243
HitoriKoishi <709917580@qq.com>
2025-01-22 14:45:16 +0800

    feat：加入每个作者问题解决率和高质量作者的分析功能

--- src/issue_pr_analysis.py
+++ src/issue_pr_analysis.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+from collections import Counter
 
 
 # 读取JSON数据
@@ -25,9 +26,17 @@ def analyze_issue_resolution(file_path):
     total_resolution_time = 0
     resolved_issues = 0
 
+    authors_close = [
+        issue["user"]["login"] for issue in issues if issue["state"] == "closed"
+    ]
+    authors = [issue["user"]["login"] for issue in issues]
+    author_close_counts = Counter(authors_close)
+    author_counts = Counter(authors)
     # 遍历每个issue
     for issue in issues:
-        if issue["state"] == "closed" and issue["closed_at"]:
+        # Track the author
+        # 分析已解决和未解决的问题
+        if issue["state"] == "closed" and issue.get("closed_at"):
             resolved_count += 1
             resolution_time = calculate_resolution_time(
                 issue["created_at"], issue["closed_at"]
@@ -48,11 +57,29 @@ def analyze_issue_resolution(file_path):
     avg_resolution_time_hours = avg_resolution_time / 3600  # 转换为小时
 
     # 打印解决率和平均解决时间
-    print(f"问题解决率: {resolution_rate:.2f}%")
+    print(f"解决率: {resolution_rate:.2f}%")
     print(f"平均解决时间: {avg_resolution_time_hours:.2f} 小时")
 
+    # 问题解决率大于等于75%视为提出高质量问题作者
+    high_efficiency_author = 0
 
-# 主函数
-if __name__ == "__main__":
-    file_path = "./data/issue_info.json"
-    analyze_issue_resolution(file_path)
+    # 打印作者解决情况
+    print("\n作者问题解决率:")
+    for author, count in author_counts.items():
+        author_close_count = author_close_counts.get(author, 0)
+
+        author_resolution_rate = (
+            (author_close_count / count) * 100 if author_close_count > 0 else 0
+        )
+        if author_resolution_rate > 75:
+            high_efficiency_author += 1
+
+        print(f"{author}: 问题解决率:{author_resolution_rate:.2f}%")
+
+    high_efficiency_author_rate = high_efficiency_author / len(author)
+    print(f"高质量作者比例:{high_efficiency_author_rate:.2f}%")
+
+
+# 示例：调用函数并分析数据
+file_path = "./data/issue_info.json"
+analyze_issue_resolution(file_path)
